ed107ea9c9ea345630be129e081d8f35d784f026
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-03-24 17:43:08 +0000

    basics commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountEmail = "ps@gmail.com";
+var accountPassword = "220192";
+accountCity = "Pune";
+let accountState;
+
+//accountId = 67890; -- const values can not be changed
+accountEmail = "rd@gmail.com";
+accountPassword = "090991";
+accountCity = "Mumbai";
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
