6ea344f9cb72211e8ff7d5949ce049b112a1f273
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-03-25 04:36:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"//treat all JS code as a newer version
+
+//alert(3 + 3); // we are using node js, not browser
+
+let name = "Priyanka";
+let age = 33;
+let isLoggedIn = true;
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof 33);
+console.log(typeof "Priya");
+console.log(typeof false);
+
+
+console.log("****************************");
+
+
+console.log(typeof Symbol);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
