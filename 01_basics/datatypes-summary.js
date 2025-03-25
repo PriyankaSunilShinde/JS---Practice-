1a7082f11c23b15c2ca6e38a0c2f32efc96a9667
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-03-25 10:13:12 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive Data types
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100;
+// const scoreValue = 100.3
+// const isLoggedIn = false;
+// let userEmail;
+
+//const outsideTemp = null;
+//console.log(typeof outsideTemp); //Output - object
+
+// const id = Symbol("123 ")
+// const anotherId = Symbol("123");
+// console.log(id == anotherId);
+
+// const bigNumber = 22553223655232n
+// console.log(typeof bigNumber);
+
+
+// Non- Primitive/ Reference Types
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "Naagraj", "Doga"];
+console.log(typeof heros); // output - object
+
+
+let myObj = {
+    namr: "Priyanka",
+    age: 33
+}
+console.log(typeof myObj); // output - object
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof myFunction); // output - function
+
+
+
