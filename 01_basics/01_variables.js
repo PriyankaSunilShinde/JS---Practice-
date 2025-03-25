2e925a169213274235e4a957e32a8bd73776f467
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-03-25 04:37:00 +0000

    notes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,6 +9,8 @@ accountEmail = "rd@gmail.com";
 accountPassword = "090991";
 accountCity = "Mumbai";
 
+/*Prefer not to use var because of issue in block scope and function scope*/
+
 
 console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
