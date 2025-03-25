deb554b1a73357f7b7ebaa2f4011237b271d8bec
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-03-25 05:37:27 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+//let score = 33;
+//let score = "33abc";
+//let score = null;
+//let score = undefined;
+//let score = true;
+//let score = false;
+// let score = "Priyanka";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof NaN);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+//let isLoggedIn = 0;
+//let isLoggedIn = 1; 
+//let isLoggedIn = "";
+//let isLoggedIn = "Priya";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 =>  true
+// 0 =>  false
+// " " => false
+// "Priya" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+ 
+
+
+
+
+
