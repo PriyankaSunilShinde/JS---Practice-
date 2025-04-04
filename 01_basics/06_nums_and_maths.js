a841045a5ea4bea81283d6a532e36f443d1921bf
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-04-04 11:27:27 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+// +++++++++++++++++  Numbers ++++++++++++++++++++++++++
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const anotherNumber = 123.85734;
+// console.log(anotherNumber.toPrecision(4));
+
+// const otherNumber = 1000000;
+// console.log(otherNumber.toLocaleString()); //used in USA
+// console.log(otherNumber.toLocaleString("en-IN")); // used in India
+
+//++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(7.8));
+// console.log(Math.round(3.6));
+// console.log(Math.sqrt(49));
+// console.log(Math.min(8,2,9,4));
+// console.log(Math.max(8,2,9,4));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor((Math.random()*10) + 1));
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
