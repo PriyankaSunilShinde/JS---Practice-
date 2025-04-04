9ea7bdf6a1d6d0b2ca756ee8fcea7e5108f4a841
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-04-04 10:14:23 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,50 @@
+// const name = "Priyanka";
+// const repoCount = 2;
+
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ("PriyankaShinde");
+
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("a"));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5,7);
+// console.log(anotherString);
+
+// const newStringOne = "    Riyansh    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://facebook.com/priyanka%20shinde";
+// console.log(url.replace("%20","-"));
+// console.log(url.includes("priyanka"));
+// console.log(url.includes("rupesh"));
+
+console.log(gameName.split("-"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
