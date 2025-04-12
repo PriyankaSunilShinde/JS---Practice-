01e7b21b0b33dd07cc04cb821e19c4999e0d1cd9
Priyanka Shinde <49552740+PriyankaSunilShinde@users.noreply.github.com>
2025-04-12 12:08:17 +0000

    array 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,57 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4];
+// console.log(myArr[2]); // 2
+
+const myHeros = ["Shaktiman", "Aaryman", "Spiderman", "Ironman",];
+// console.log(myHeros); // [ 'Shaktiman', 'Aaryman', 'Spiderman', 'Ironman' ]
+
+const myArray = new Array(22, 9, 25);
+// console.log(myArray[0]);
+
+// Array Methods
+// myArr.push(5);
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// myArr.pop();
+// myArr.unshift(9);
+// myArr.shift();
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+
+// slice 
+
+console.log("A ", myArr);
+const Arr1 = myArr.slice(1,3);
+console.log(Arr1);
+
+
+// splice
+
+console.log("B ", myArr);
+const Arr2 = myArr.splice(1,3);
+console.log(Arr2);
+console.log("C ", myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
